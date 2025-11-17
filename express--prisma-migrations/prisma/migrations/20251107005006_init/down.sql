e851e85b133d012e2970d8846ea3633a794677c6
Victor Lira <104510669+victorlirafront@users.noreply.github.com>
2025-11-16 23:12:32 -0300

    remove non used projects

--- express--prisma-migrations/prisma/migrations/20251107005006_init/down.sql
+++
@@ -1,9 +0,0 @@
--- Rollback: Drop the users table
--- Este arquivo documenta como reverter esta migration manualmente
-
--- DropIndex
-DROP INDEX IF EXISTS "users_email_key";
-
--- DropTable
-DROP TABLE IF EXISTS "users";
-
